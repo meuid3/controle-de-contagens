0ab78bb7a24062c5e4e5997ec82d42b91f3534e0
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-18 01:06:43 -0300

    add: Adicionado camada de serviço

---
+++ src/services/tabelaService.js
@@ -0,0 +1,16 @@
+class TabelaService {
+
+  constructor({tabelaRepository}) {
+    this.tabelaRepository = tabelaRepository
+  }
+
+  async find(idTabela) {
+    return this.tabelaRepository.find(idTabela)
+  }
+
+  async create(data) {
+    return this.tabelaRepository.create(data)
+  }
+}
+
+module.exports = TabelaService;
