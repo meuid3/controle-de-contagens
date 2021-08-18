eadf54e4385542b956ed31ce58ef7803f82136cb
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-18 00:45:04 -0300

    add: Adicionado camada de repositorio

---
+++ src/repositories/tabelaRepository.js
@@ -0,0 +1,16 @@
+class TabelaRepository {
+  
+  constructor(table) {
+    this.table = table
+  }
+
+  find(idTabela) {
+    
+  }
+
+  async create(data) {
+
+  }
+}
+
+module.exports = TabelaRepository;
