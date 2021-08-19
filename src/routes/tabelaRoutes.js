df142fc3b109e0cc300b56d7efe21e42d47e6c6e
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-18 23:20:03 -0300

    refactory: Removendo mock de retorno da rota para chamada da instância de serviços

--- src/routes/tabelaRoutes.js
+++ src/routes/tabelaRoutes.js
@@ -1,9 +1,12 @@
 const {Router} = require('express')
+const {generateInstance} = require('./../factories/tabelaFactory')
 
 const tabelaRouter = Router()
 
-tabelaRouter.get('/', (request, response)=> {
-  response.json({message: "Rota de Tabela"})
+tabelaRouter.get('/', async (request, response) => {
+  const tabela = generateInstance()
+  let result = await tabela.find(1)
+  response.json(result)
 })
 
 module.exports = {tabelaRouter}
