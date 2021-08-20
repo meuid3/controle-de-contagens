bbb2e2864ed78e1a5e01fb4eb9d86c76f807e4d8
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-20 01:28:20 -0300

    refactoring: simplificando instância do banco de dados na camada de repositorio

--- src/repositories/tabelaRepository.js
+++ src/repositories/tabelaRepository.js
@@ -1,10 +1,9 @@
-const {DB} = require('../../db/db')
+const db = require('../../db/db')
 
 class TabelaRepository {
   
   async find(idTabela) {
     try {
-      const db = await new DB().getConnection()
       const query = `SELECT * FROM public.tabela WHERE id = $1`
       const result = await db.query(query, [idTabela])
       return result.rows
@@ -15,14 +14,13 @@ class TabelaRepository {
   }
 
   async getAll() {
-    const db = await new DB().getConnection()
+    //const db = await new DB().getConnection()
     const result = await db.query("select * from tabela")
     return result.rows
   }
 
   async create({nome, schema} = dados) {
     try {
-      const db = await new DB().getConnection()
       const query = `INSERT INTO tabela (nome, schema) VALUES ($1, $2) RETURNING *`
       const result = await db.query(query,[nome, schema])
       return result.rows
@@ -34,7 +32,6 @@ class TabelaRepository {
 
   async update({id, nome, schema} = dados) {
     try {
-      const db = await new DB().getConnection()
       const query = `UPDATE public.tabela SET nome= $1, schema= $2 WHERE id = $3 RETURNING *`
       const result = await db.query(query, [nome, schema, id])
       return result.rows
@@ -46,7 +43,6 @@ class TabelaRepository {
 
   async delete(idTabela) {
     try {
-      const db = await new DB().getConnection()
       const query = `DELETE FROM public.tabela WHERE id = $1 RETURNING *`
       const result = await db.query(query, [idTabela])
       return result.rows
