c2879624c75c0b51b12dd2ba6feb05019ac84728
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-20 00:25:01 -0300

    refactoring: Adicionado métodos find, getAll, update e delete para manipulação dos dados da tabela

--- src/repositories/tabelaRepository.js
+++ src/repositories/tabelaRepository.js
@@ -2,23 +2,22 @@ const {DB} = require('../../db/db')
 
 class TabelaRepository {
   
-  constructor(dados) {
-    this.dados = dados
-  }
-
-  getDados() {
-    return this.dados
-  }
-
   async find(idTabela) {
     try {
       const db = await new DB().getConnection()
-      const result = await db.query("select * from tabela")
+      const query = `SELECT * FROM public.tabela WHERE id = $1`
+      const result = await db.query(query, [idTabela])
       return result.rows
-    } 
-    catch( error ) {
+    }
+    catch(error) {
       return error
     }
+  }
+
+  async getAll() {
+    const db = await new DB().getConnection()
+    const result = await db.query("select * from tabela")
+    return result.rows
   }
 
   async create({nome, schema} = dados) {
@@ -28,7 +27,31 @@ class TabelaRepository {
       const result = await db.query(query,[nome, schema])
       return result.rows
     } 
-    catch( error ) {
+    catch(error) {
+      return error
+    }
+  }
+
+  async update({id, nome, schema} = dados) {
+    try {
+      const db = await new DB().getConnection()
+      const query = `UPDATE public.tabela SET nome= $1, schema= $2 WHERE id = $3 RETURNING *`
+      const result = await db.query(query, [nome, schema, id])
+      return result.rows
+    }
+    catch(error) {
+      return error
+    }
+  }
+
+  async delete(idTabela) {
+    try {
+      const db = await new DB().getConnection()
+      const query = `DELETE public.tabela where id = $1 RETURNING *`
+      const result = await db.query(query, [idTabela])
+      return result.rows
+    }
+    catch(error) {
       return error
     }
   }
