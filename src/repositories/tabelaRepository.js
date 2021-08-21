6f1c178905a5b3785a6501cd4993790112ec5c64
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-21 03:00:57 -0300

    refactoring: Remoção dos métodos genéricos para extender os métodos da classe Model

--- src/repositories/tabelaRepository.js
+++ src/repositories/tabelaRepository.js
@@ -1,57 +1,15 @@
-const db = require('../../db/db')
+const Model = require('./Model')
 
-class TabelaRepository {
-  
-  async find(idTabela) {
-    try {
-      const query = `SELECT * FROM public.tabela WHERE id = $1`
-      const result = await db.query(query, [idTabela])
-      return result.rows
-    }
-    catch(error) {
-      return error
-    }
+class TabelaRepository extends Model{
+
+  constructor() {
+    super({
+      properties: ['nome', 'schema', 'id'],
+      table: "tabela",
+      schema: "public",
+      primaryKey: "id"
+    })
   }
-
-  async getAll() {
-    //const db = await new DB().getConnection()
-    const result = await db.query("select * from tabela")
-    return result.rows
-  }
-
-  async create({nome, schema} = dados) {
-    try {
-      const query = `INSERT INTO tabela (nome, schema) VALUES ($1, $2) RETURNING *`
-      const result = await db.query(query,[nome, schema])
-      return result.rows
-    } 
-    catch(error) {
-      return error
-    }
-  }
-
-  async update({id, nome, schema} = dados) {
-    try {
-      const query = `UPDATE public.tabela SET nome= $1, schema= $2 WHERE id = $3 RETURNING *`
-      const result = await db.query(query, [nome, schema, id])
-      return result.rows
-    }
-    catch(error) {
-      return error
-    }
-  }
-
-  async delete(idTabela) {
-    try {
-      const query = `DELETE FROM public.tabela WHERE id = $1 RETURNING *`
-      const result = await db.query(query, [idTabela])
-      return result.rows
-    }
-    catch(error) {
-      return error
-    }
-  }
-
 }
 
 module.exports = TabelaRepository
