57d894f586b9c9d265bdea8778e30fa45e102943
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-24 00:16:12 -0300

    add: declaração do módulo de rotas de funcionalidade

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const {Router} = require('express')
 const { moduloRouter } = require('./routes/moduloRoutes')
 const { tabelaRouter } = require('./routes/tabelaRoutes')
+const { funcionalidadeRouter } = require('./routes/funcionalidadeRoutes')
 
 const routes = Router()
 
 routes.use('/tabela', tabelaRouter)
 routes.use('/modulo', moduloRouter)
+routes.use('/funcionalidade', funcionalidadeRouter)
 
 module.exports = {routes}
