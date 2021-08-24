98a4255e58fdb0b3dc4dc5da61d5468885dfe8bf
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-24 00:03:14 -0300

    add: Adicionado factory para entidade de Funcionalidade

---
+++ src/factories/funcionalidadeFactory.js
@@ -0,0 +1,8 @@
+const FuncionalidadeRepository = require('../repositories/funcionalidadeRepository')
+const FuncionalidadeService = require('../services/funcionalidadeService')
+
+const generateInstanceFuncionalidade = (data) => {
+  const funcionalidadeRepository = new FuncionalidadeRepository(data)
+  const funcionalidadeService = new FuncionalidadeService(funcionalidadeRepository)
+  return {funcionalidadeService}
+}
