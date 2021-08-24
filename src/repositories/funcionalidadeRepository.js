03405b538198f6791a11edc9454da4b07759e58f
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-23 23:41:24 -0300

    add: Adicionado camada de repositorio para funcionalidade

---
+++ src/repositories/funcionalidadeRepository.js
@@ -0,0 +1,26 @@
+const Model = require('./Model')
+
+class FuncionalidadeRepository extends Model {
+  constructor() {
+    super({
+      properties: [
+        {
+          name: 'id',
+          isNull: false
+        },
+        {
+          name: 'nome',
+          isNull: false
+        },
+        {
+          name: 'modulo_id',
+          isNull: false
+        }
+      ],
+      table: "funcionalidade",
+      schema: "public",
+      primaryKey: "id",
+      generateExternalId: false
+    })
+  }
+}
