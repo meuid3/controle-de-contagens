f56fa8a186f28671bc6b32f0e91d3019c41d2656
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-24 00:02:06 -0300

    add: Adicionado camada de Service para entidade de Funcionalidade

---
+++ src/services/funcionalidadeService.js
@@ -0,0 +1,40 @@
+const Funcionalidade = require('../entities/funcionalidade')
+
+class FuncionalidadeService {
+  constructor(funcionalidadeRepository) {
+    this.funcionalidadeRepository = funcionalidadeRepository
+  }
+
+  async getAll() {
+    return await this.funcionalidadeRepository.getAll()
+  }
+
+  async find(idFuncionalidade) {
+    if(idFuncionalidade) {
+      if(!isNaN(idFuncionalidade)) {
+        return await this.funcionalidadeRepository.find(idFuncionalidade)
+      } else {
+        return {message: `o id informado é inválido!`}
+      }
+    }
+  }
+
+  async create(data) {
+    const funcionalidade = new Funcionalidade(data)
+    if(funcionalidade.isValid().status) {
+      return await this.funcionalidadeRepository.create(funcionalidade)
+    }
+
+    return {message: funcionalidade.isValid().campos}
+  }
+
+  async update(data) {
+      return await this.funcionalidadeRepository.update(data)
+  }
+
+  async delete(idModulo) {
+    return await this.funcionalidadeRepository.delete(idModulo)
+  }
+}
+
+module.exports = FuncionalidadeService
