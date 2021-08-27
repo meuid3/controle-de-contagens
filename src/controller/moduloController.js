de099699c00c9a74fff3d91c57ae3c68759994c5
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-26 23:57:20 -0300

    refactoring: Adicionando tratamento de erros no controlador de Modulo

--- src/controller/moduloController.js
+++ src/controller/moduloController.js
@@ -3,35 +3,65 @@ const {generateInstanceModulo} = require('./../factories/moduloFactory')
 class Modulocontroller {
 
   async find(request, response) {
-    const modulo = generateInstanceModulo()
-    const {id} = request.params
-    let result = await modulo.find(id)
-    response.json(result)
+    try {
+      const modulo = generateInstanceModulo()
+      const {id} = request.params
+      let result = await modulo.find(id)
+      response.status(200).json(result)
+    } catch(error) {
+      response.status(500).json({
+        message: error.mesage
+      })
+    }
   }
 
   async getAll(request, response) {
-    const modulo = generateInstanceModulo()
-    let result = await modulo.getAll()
-    response.json(result)
+    try {
+      const modulo = generateInstanceModulo()
+      let result = await modulo.getAll()
+      response.status(200).json(result)
+    } catch(error) {
+      response.status(500).json({
+        message: error.error
+      })
+    }
   }
 
   async create(request, response) {
-    const modulo = generateInstanceModulo()
-    let result = await modulo.create(request.body)
-    response.status(200).json(result)
+    try {
+      const modulo = generateInstanceModulo()
+      let result = await modulo.create(request.body)
+      response.status(201).json(result)
+    } catch(error) {
+      response.status(500).json({
+        message: error.message
+      })
+    }
   }
 
   async update(request, response) {
-    const modulo = generateInstanceModulo()
-    let result = await modulo.update(request.body)
-    response.status(200).json(result)
+    try {
+      const modulo = generateInstanceModulo()
+      let result = await modulo.update(request.body)
+      response.status(201).json(result)
+    } catch(error) {
+      response.status(500).json({
+        message: error.message
+      })
+    }
   }
 
   async delete(request, response) {
-    const modulo = generateInstanceModulo()
-    const {id} = request.params
-    let result = await modulo.delete(id)
-    response.status(200).json(result)
+    try {
+      const modulo = generateInstanceModulo()
+      const {id} = request.params
+      let result = await modulo.delete(id)
+      response.status(200).json(result)
+    } catch(error) {
+      response.status(500).json({
+        message: error.message
+      })
+    }
   }
 
 }
