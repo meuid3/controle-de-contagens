3c275c0bd7e92b92dad295fff94bb720bc5197b3
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-26 21:22:49 -0300

    refactoring: Removendo responsabilidade de processamento de retorno das rotas de Tabela para controlador

--- src/routes/tabelaRoutes.js
+++ src/routes/tabelaRoutes.js
@@ -1,38 +1,12 @@
 const {Router} = require('express')
-const {generateInstance} = require('./../factories/tabelaFactory')
+const TabelaController = require('../controller/tabelaController')
 
 const tabelaRouter = Router()
 
-tabelaRouter.get('/', async (request, response) => {
-  const tabela = generateInstance()
-  let result = await tabela.getAll()
-  response.json(result)
-})
-
-tabelaRouter.get('/:id', async (request, response) => {
-  const tabela = generateInstance()
-  const {id} = request.params
-  let result = await tabela.find(id)
-  response.json(result)
-})
-
-tabelaRouter.post('/create', async (request, response) => {
-  const tabela = generateInstance()
-  let result = await tabela.create(request.body)
-  response.status(200).json(result)
-})
-
-tabelaRouter.post('/update', async (request, response) => {
-  const tabela = generateInstance()
-  let result = await tabela.update(request.body)
-  response.status(200).json(result)
-})
-
-tabelaRouter.post('/excluir/:id', async (request, response) => {
-  const tabela = generateInstance()
-  const {id} = request.params
-  let result = await tabela.delete(id)
-  response.status(200).json(result)
-})
+tabelaRouter.get('/', TabelaController.getAll)
+tabelaRouter.get('/:id', TabelaController.find)
+tabelaRouter.post('/create', TabelaController.create)
+tabelaRouter.post('/update', TabelaController.update)
+tabelaRouter.post('/excluir/:id', TabelaController.delete)
 
 module.exports = {tabelaRouter}
