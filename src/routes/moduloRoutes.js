fb8ef784e5666145c7f044c326ed15b36db39f0e
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-26 21:10:34 -0300

    refactoring: Removendo responsabilidade de processamento de retorno das rotas para controlador

--- src/routes/moduloRoutes.js
+++ src/routes/moduloRoutes.js
@@ -1,38 +1,12 @@
 const {Router} = require('express')
-const {generateInstanceModulo} = require('./../factories/moduloFactory')
+const ModuloController = require('../controller/moduloController')
 
 const moduloRouter = Router()
 
-moduloRouter.get('/', async (request, response) => {
-  const modulo = generateInstanceModulo()
-  let result = await modulo.getAll()
-  response.json(result)
-})
-
-moduloRouter.get('/:id', async (request, response) => {
-  const modulo = generateInstanceModulo()
-  const {id} = request.params
-  let result = await modulo.find(id)
-  response.json(result)
-})
-
-moduloRouter.post('/create', async (request, response) => {
-  const modulo = generateInstanceModulo()
-  let result = await modulo.create(request.body)
-  response.status(200).json(result)
-})
-
-moduloRouter.post('/update', async (request, response) => {
-  const modulo = generateInstanceModulo()
-  let result = await modulo.update(request.body)
-  response.status(200).json(result)
-})
-
-moduloRouter.post('/excluir/:id', async (request, response) => {
-  const modulo = generateInstanceModulo()
-  const {id} = request.params
-  let result = await modulo.delete(id)
-  response.status(200).json(result)
-})
+moduloRouter.get('/', ModuloController.getAll)
+moduloRouter.get('/:id', ModuloController.find)
+moduloRouter.post('/create', ModuloController.create)
+moduloRouter.post('/update', ModuloController.update)
+moduloRouter.post('/excluir/:id', ModuloController.delete)
 
 module.exports = {moduloRouter}
