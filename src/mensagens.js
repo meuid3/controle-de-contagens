6e2c43f96a768199e3e91d5f99c93cb052cd158f
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-26 22:47:45 -0300

    refactoring: Adicionando nova mensgem de erro no arquivo de Mensagens

--- src/mensagens.js
+++ src/mensagens.js
@@ -1,3 +1,4 @@
 module.exports = Mensagens = {
-  PARAMETRO_INVALIDO:'O parâmetro informado é inválido'
+  PARAMETRO_INVALIDO:'O parâmetro informado é inválido',
+  PARAMETRO_ID_OBRIGATORIO: 'O parâmetro (id) é obrigatório'
 }
