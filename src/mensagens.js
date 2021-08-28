bb82ba08fdfc2df5cd4c4e9f38be976b4f3215d2
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-28 18:10:24 -0300

    refactoring: Adicionado mensagem de erro para FuncionalidadeTabelas

--- src/mensagens.js
+++ src/mensagens.js
@@ -1,5 +1,13 @@
 module.exports = Mensagens = {
+  /**
+   * Mensagens Genéricas
+   */
   PARAMETRO_INVALIDO:'O parâmetro informado é inválido',
   PARAMETRO_ID_OBRIGATORIO: 'O parâmetro (id) é obrigatório',
-  REGISTRO_NAO_ENCONTRADO: 'O registro {0} não foi localizado na base de dados.'
+  REGISTRO_NAO_ENCONTRADO: 'O registro {0} não foi localizado na base de dados.',
+
+  /**
+   * FuncionalidadeTabelas
+   */
+  TABELA_JA_VINCULADA_A_FUNCIONALIDADE: 'A tabela informada já foi vinculada na funcionalidade.'
 }
