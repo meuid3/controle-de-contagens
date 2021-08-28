fb2fe977a585ebdb7187ab833e81ea7eb80e60e6
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-28 13:50:44 -0300

    refactoring: declarando rota de funcionalidadeTabela no arquivo principal de rotas

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const {Router} = require('express')
 const { moduloRouter } = require('./routes/moduloRoutes')
 const { tabelaRouter } = require('./routes/tabelaRoutes')
 const { funcionalidadeRouter } = require('./routes/funcionalidadeRoutes')
+const { funcionalidadeTabelaRouter } = require('./routes/funcionalidadeTabelaRoutes')
 
 const routes = Router()
 
 routes.use('/tabela', tabelaRouter)
 routes.use('/modulo', moduloRouter)
 routes.use('/funcionalidade', funcionalidadeRouter)
+routes.use('/funcionalidadeTabela', funcionalidadeTabelaRouter)
 
 module.exports = {routes}
