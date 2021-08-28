c89b3c4ea5b1728aada882b2a7a925bffca26cb5
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-28 01:21:55 -0300

    refactoring: Adicionado chamada do metodo de vincular tabela e funcionalidade no controller de Funcionalidade

--- src/controller/funcionalidadeController.js
+++ src/controller/funcionalidadeController.js
@@ -8,6 +8,7 @@ class FuncionalidadeController {
       const {id} = request.params
       let result = await funcionalidade.find(id)
       response.json(result)
+    
     } catch(error) {
       response.status(500).json({
         message: error.message
@@ -20,6 +21,7 @@ class FuncionalidadeController {
       const funcionalidade = generateInstanceFuncionalidade()
       let result = await funcionalidade.getAll()
       response.status(200).json(result)
+    
     } catch(error) {
       response.status(500).json({
         message: error.message
@@ -32,6 +34,7 @@ class FuncionalidadeController {
       const funcionalidade = generateInstanceFuncionalidade()
       let result = await funcionalidade.create(request.body)
       response.status(200).json(result)
+    
     } catch(error) {
       response.status(500).json({
         message: error.message
@@ -44,6 +47,7 @@ class FuncionalidadeController {
       const funcionalidade = generateInstanceFuncionalidade()
       let result = await funcionalidade.update(request.body)
       response.status(200).json(result)
+    
     } catch(error){
       response.status(500).json({
         message: error.message
@@ -57,6 +61,21 @@ class FuncionalidadeController {
       const {id} = request.params
       let result = await funcionalidade.delete(id)
       response.status(200).json(result)
+    
+    } catch(error) {
+      response.status(500).json({
+        message: error.message
+      })
+    }
+  }
+
+  async vincularTabelaFuncionalidade(request, response) {
+    try {
+      const funcionalidade = generateInstanceFuncionalidade()
+      const dados = request.body
+      const result = await funcionalidade.vincularTabelaFuncionalidade(dados)
+      response.status(201).json(result)
+
     } catch(error) {
       response.status(500).json({
         message: error.message
