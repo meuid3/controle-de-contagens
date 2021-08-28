e2609f95ab8447f3f00147695e1be71bb67d2b73
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-28 01:22:20 -0300

    refactoring: Adicionado chamada do metodo de vincular tabela e funcionalidade na rota de Funcionalidade

--- src/routes/funcionalidadeRoutes.js
+++ src/routes/funcionalidadeRoutes.js
@@ -8,5 +8,6 @@ funcionalidadeRouter.get('/:id', FuncionalidadeController.find)
 funcionalidadeRouter.post('/create', FuncionalidadeController.create)
 funcionalidadeRouter.post('/update', FuncionalidadeController.update)
 funcionalidadeRouter.post('/excluir/:id', FuncionalidadeController.delete)
+funcionalidadeRouter.post('/vincularTabela', FuncionalidadeController.vincularTabelaFuncionalidade)
 
 module.exports = {funcionalidadeRouter}
