64f517ff8dbafe014c203fcd1895a4f6eb87f298
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-28 01:17:50 -0300

    refactoring: Definindo nome mais adequado para factory de Tabela

--- src/factories/tabelaFactory.js
+++ src/factories/tabelaFactory.js
@@ -1,10 +1,10 @@
 const TabelaService = require('./../services/tabelaService')
 const TabelaRepository = require('./../repositories/tabelaRepository')
 
-const generateInstance = (data) => {
+const generateTabelaInstance = (data) => {
   const tabelaRepository = new TabelaRepository(data)
   const tabelaService = new TabelaService(tabelaRepository)
   return tabelaService;
 }
 
-module.exports = {generateInstance}
+module.exports = {generateTabelaInstance}
