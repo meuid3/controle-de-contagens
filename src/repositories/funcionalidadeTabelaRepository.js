874af2e2b71cf9df7aef307e1c08b03256feaa8b
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-28 17:11:54 -0300

    refactoring: Adicionado método que retorna a funcionalidade com tabelas vinculads por id no repository de FuncionalidadeTabela

--- src/repositories/funcionalidadeTabelaRepository.js
+++ src/repositories/funcionalidadeTabelaRepository.js
@@ -1,4 +1,5 @@
 const Model = require('./Model')
+const Mensagens = require('../mensagens')
 
 class FuncionalidadeTabelaRepository extends Model {
 
@@ -23,6 +24,22 @@ class FuncionalidadeTabelaRepository extends Model {
       primaryKey: 'id',
       generateExternalId: false
     })
+  }
+
+  async getFuncionaliadadeTabelaById(idFuncionalidade) {
+    if(idFuncionalidade) {
+      const query = `
+        SELECT *
+        FROM funcionalidade_tabela ft 
+        INNER JOIN funcionalidade f ON ft.funcionalidade_id  = f.id
+        INNER JOIN tabela t ON ft.tabela_id  = t.id
+        WHERE f.id = $1
+      `
+      const result = await this.db.query(query,[idFuncionalidade])
+      return result.rows
+    }
+
+    throw new Error(Mensagens.PARAMETRO_ID_OBRIGATORIO)
   }
 }
 
