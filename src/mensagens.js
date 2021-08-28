d112751f0829b6bc7e021a3bbf4e261f6aa1a15f
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-28 01:15:50 -0300

    refactoring: Adicionado mensagem registro não encontrado

--- src/mensagens.js
+++ src/mensagens.js
@@ -1,4 +1,5 @@
 module.exports = Mensagens = {
   PARAMETRO_INVALIDO:'O parâmetro informado é inválido',
-  PARAMETRO_ID_OBRIGATORIO: 'O parâmetro (id) é obrigatório'
+  PARAMETRO_ID_OBRIGATORIO: 'O parâmetro (id) é obrigatório',
+  REGISTRO_NAO_ENCONTRADO: 'O registro {0} não foi localizado na base de dados.'
 }
