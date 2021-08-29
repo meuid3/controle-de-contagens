00e2848b519a3c6b17f6891e6adc02ab09bf0757
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-29 02:40:58 -0300

    refactoring: Remoção da rota de vincularTabelaFuncionalidade e adicionado rota de retorno de funcionalidadeCompleta

--- src/routes/funcionalidadeRoutes.js
+++ src/routes/funcionalidadeRoutes.js
@@ -8,6 +8,6 @@ funcionalidadeRouter.get('/:id', FuncionalidadeController.find)
 funcionalidadeRouter.post('/create', FuncionalidadeController.create)
 funcionalidadeRouter.post('/update', FuncionalidadeController.update)
 funcionalidadeRouter.post('/excluir/:id', FuncionalidadeController.delete)
-funcionalidadeRouter.post('/vincularTabela', FuncionalidadeController.vincularTabelaFuncionalidade)
+funcionalidadeRouter.get('/funcionalidade-completa/:id', FuncionalidadeController.getFuncionalidadeCompletaById)
 
 module.exports = {funcionalidadeRouter}
