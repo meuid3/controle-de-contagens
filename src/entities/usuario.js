0aa82046a2ecde446016d2adb0889aecbb8f751b
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-29 22:52:43 -0300

    add: Adicionado Entidade Usuario

---
+++ src/entities/usuario.js
@@ -0,0 +1,28 @@
+class Usuario {
+  
+  constructor({id, nome, email, senha}) {
+    this.id = id
+    this.nome = nome
+    this.email = email
+    this.senha = senha
+  }
+
+  isValid() {
+    const propriedades = Object.getOwnPropertyNames(this)
+    const propriedadesInvalidas = propriedades
+      .map( propriedade => {
+        if(propriedade !== 'id') {
+          return !!this[propriedade] ? null : `${propriedade} não informado`
+        }
+      })
+      .filter(propriedade => !!propriedade)
+
+    return {
+      status: propriedadesInvalidas.length == 0,
+      property: propriedadesInvalidas
+    }
+  }
+
+}
+
+module.exports = Usuario
