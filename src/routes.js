026604cd3499e48d5fa23f9e3748bc4ae54703bf
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-29 22:58:33 -0300

    add: Declarando rota de usuário no arquivo de rotas principal

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const { moduloRouter } = require('./routes/moduloRoutes')
 const { tabelaRouter } = require('./routes/tabelaRoutes')
+const { usuarioRouter} = require('./routes/usuarioRoutes')
 const { funcionalidadeRouter } = require('./routes/funcionalidadeRoutes')
 const { funcionalidadeTabelaRouter } = require('./routes/funcionalidadeTabelaRoutes')
 
@@ -8,6 +9,7 @@ const routes = Router()
 
 routes.use('/tabela', tabelaRouter)
 routes.use('/modulo', moduloRouter)
+routes.use('/usuario', usuarioRouter)
 routes.use('/funcionalidade', funcionalidadeRouter)
 routes.use('/funcionalidadeTabela', funcionalidadeTabelaRouter)
 
