edf9168708827984172ef8c3682bf9be21e0d145
Marcos Moraes Pereira <meuid3@gmail.com>
2021-08-30 22:47:11 -0300

    refactoring:  Adicionado validação para cadastro de novos usuários

--- src/services/usuarioService.js
+++ src/services/usuarioService.js
@@ -1,6 +1,7 @@
 const {hash} = require('bcryptjs')
 const Mensagens = require('../mensagens')
 const Usuario = require('./../entities/usuario')
+const UsuarioRepository = require('../repositories/usuarioRepository')
 
 class UsuarioService {
 
@@ -21,14 +22,23 @@ class UsuarioService {
   }
 
   async create(data) {
-    const senhaCriptografada = await hash(data.senha, 8)
-    data.senha = senhaCriptografada
-    const usuarioCriado = await this.usuarioRepository.create(data)
 
-    delete usuarioCriado.id
-    delete usuarioCriado.senha
-    
-    return usuarioCriado
+    const usuario = new Usuario(data)
+    if(usuario.isValid().status) {
+
+      await this._verificaSePossuiRegistro(usuario)
+
+      const senhaCriptografada = await hash(data.senha, 8)
+      data.senha = senhaCriptografada
+      const usuarioCriado = await this.usuarioRepository.create(data)
+  
+      delete usuarioCriado.id
+      delete usuarioCriado.senha
+      
+      return usuarioCriado
+    }
+
+    throw new  Error(usuario.isValid().property)
   }
 
   async update(data) {
@@ -45,6 +55,14 @@ class UsuarioService {
     }
     
     throw new Error(Mensagens.PARAMETRO_INVALIDO)
+  }
+
+  async _verificaSePossuiRegistro({email} = usuario) {
+    const usuarioRepository = new UsuarioRepository()
+    const usuarioCadastrado = await usuarioRepository.buscaUsuarioPorEmail(email)
+
+    if(usuarioCadastrado.length > 0)
+      throw new Error(Mensagens.O_USUARIO_JA_EXISTE_CADASTRO)
   }
 }
 
